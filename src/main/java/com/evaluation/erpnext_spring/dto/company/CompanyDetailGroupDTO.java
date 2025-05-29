435b191fa8d05ec880e288645c4a88fb3f8da27c
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-05-29 08:53:13 +0300

    ok

--- src/main/java/com/evaluation/erpnext_spring/dto/company/CompanyDetailGroupDTO.java
+++
@@ -1,8 +0,0 @@
-package com.evaluation.erpnext_spring.dto.company;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class CompanyDetailGroupDTO {
-    @JsonProperty("data")
-    private CompanyDetailDTO data;
-}
