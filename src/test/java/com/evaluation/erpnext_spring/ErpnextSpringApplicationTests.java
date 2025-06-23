7546f98e4b92f1da7d18e8bd3c40884c1b152a95
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-06-23 11:35:48 +0300

    Update ErpnextSpringApplicationTests.java

--- src/test/java/com/evaluation/erpnext_spring/ErpnextSpringApplicationTests.java
+++ src/test/java/com/evaluation/erpnext_spring/ErpnextSpringApplicationTests.java
@@ -3,11 +3,13 @@ package com.evaluation.erpnext_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ErpnextSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
